474ab2633dcc35b017cf877d67092e68eebb4730
Nexterday <nexterday@users.noreply.github.com>
2025-07-01 15:47:48 +0200

    Fixes EMDL-1368

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2025052100;
+$plugin->version    = 2025070100;
 $plugin->requires   = 2017111300;
 $plugin->component  = 'quiz_filedownloader';
 $plugin->maturity   = MATURITY_STABLE;
